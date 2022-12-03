5cf874f993835fd3ed2f18da81a5d6f4bbe5d78d
anjalipoudel <sabnom77poudel@gmail.com>
2022-12-03 23:46:30 +0545

    init

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:portfolioapp/information_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Portfolio App"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // push to next page
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const InformationPage()));
+          },
+          child: const Text("Get Started"),
+        ),
+      ),
+    );
+  }
+}
